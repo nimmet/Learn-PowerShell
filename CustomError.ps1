f4f564f20273f7eed8cc98b7a7ac0f55cf95d47a
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-25 19:42:53 +0300

    First powershell script

---
+++ CustomError.ps1
@@ -0,0 +1,14 @@
+Try {
+Write-Host('The first command that you entered was valid.')
+} Catch {
+
+Write-Host('The first command that you entered was invalid.')
+}
+
+Try {
+Write-Host('The second command that you entered was valid.')
+} Catch {
+Write-Host('The second command that you entered was invalid.')
+}
+
+Write-Host("Hello`n world")
---
+++ hello.py
@@ -0,0 +1,3 @@
+
+print(f"Helo World")
+print("What are you doing?")
