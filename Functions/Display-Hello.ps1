d61025aed3ca805446d62db9fff979ad109032e2
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-25 20:35:58 +0300

    This function can dissplay Hashtable and its elements

---
+++ Functions/Display-Hello.ps1
@@ -0,0 +1,12 @@
+function DisplayHello {
+    cls
+    Write-Host("Hello World")
+
+    $Months = @{1="Jan";2="Feb";3="Mar";4="Apr";5="May";6="Jun";
+    7="Jul";8="Aug";9="Sep";10="Oct";11="Nov";12="Dec"}
+    
+    write-Host($Months)
+    Write-Host("Prnt out of the first Month: "+$Months.1)
+}
+
+DisplayHello
